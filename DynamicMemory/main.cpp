f37b26c78adf36aab259ce1c2a3bdf6f8812bd81
hawri <hawri@DESKTOP-BMM1386>
2024-06-26 14:49:22 +0300

    HW

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
-int* push_back(int arr[], int& n, const int value);
 
+int* push_back(int arr[], int& n, const int value);
+int* push_front(int arr[], int& n, const int value);
+int* insert(int arr[], int& n, const int value, const int index);
+
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
+int* erase(int arr[], int& n, const int index);
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -21,6 +27,26 @@ void main()
 	int value;
 	cout << "Введите добавляемое значение: "; cin >> value;
 	arr = push_back(arr, n, value);
+	Print(arr, n);
+
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	int index;
+	cout << "Введите индекс добавляемого значения: "; cin >> index;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr = insert(arr, n, value, index);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	arr = erase(arr, n, index);
 	Print(arr, n);
 
 	delete[]arr;
@@ -62,4 +88,50 @@ int* push_back(int arr[], int& n, const int value)
 	//После добавления элемента в конец массива,количество его элементов увеличивается на 1:
 	n++;
 	return arr;
+}
+int* push_front(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)buffer[i + 1] = arr[i];
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+int* insert(int arr[], int& n, const int value, const int index)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < index; i++)buffer[i] = arr[i];
+	for (int i = index; i < n; i++)buffer[i + 1] = arr[i];
+	//for (int i = 0; i < n; i++)
+	//{
+	//	if (i < index)buffer[i] = arr[i];
+	//	else buffer[i + 1] = arr[i];
+	//}
+	delete[]arr;
+	buffer[index] = value;
+	n++;
+	return buffer;
+}
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i + 1];
+	delete[] arr;
+	return buffer;
+}
+int* erase(int arr[], int& n, const int index)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < index; i++)buffer[i] = arr[i];
+	for (int i = index; i < n; i++)buffer[i] = arr[i + 1];
+	delete[] arr;
+	return buffer;
 }
